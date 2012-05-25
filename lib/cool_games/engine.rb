28d0ea327d888a16a0beaf67162523948533d57c
Guoliang Cao <gcao99@gmail.com>
2012-05-25 14:24:10 -0400

    Move assets from main app to the engine

--- lib/cool_games/engine.rb
+++ lib/cool_games/engine.rb
@@ -5,5 +5,9 @@ module CoolGames
     # This makes it possible to refer routes like cool_games.games_path
     engine_name 'cool_games'
 
+    # Precompile assets that are not referenced from main app
+    initializer "cool_games.assets.precompile" do |app|
+      app.config.assets.precompile += %w[]
+    end
   end
 end
